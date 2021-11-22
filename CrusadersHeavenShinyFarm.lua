6a996975e00f403c71a33a3372c0fd0e7d49ebf4
gha-lol <85558741+gha-lol@users.noreply.github.com>
2021-11-22 20:48:14 -0300

    Update CrusadersHeavenShinyFarm.lua

--- CrusadersHeavenShinyFarm.lua
+++ CrusadersHeavenShinyFarm.lua
@@ -16,6 +16,7 @@ end
 
 game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Credits"; Text = "Made by: Ghabrieel On V3rmillion"})
 while _G.shinyFarm do wait()
+    game.Players.LocalPlayer.PlayerGui:WaitForChild("Menu")
     e = game.Players.LocalPlayer.PlayerGui.Menu.Frame.StandInfo.vpf:FindFirstChildOfClass("Model")
     if e and not table.find(_G.nonShiny, e.Name) then
         game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Shiny Notification"; Text = "Got a Shiny!"})
