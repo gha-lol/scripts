bd82e57dcc4873a30ff395dfacb18cebcbe21892
gha-lol <85558741+gha-lol@users.noreply.github.com>
2025-02-14 14:14:15 -0300

    Update farminhas.lua

--- farminhas.lua
+++ farminhas.lua
@@ -71,10 +71,12 @@ tab1:CreateToggle("Auto-Farm",false,function(bool)
                 if mob and mob:FindFirstChild("HumanoidRootPart") and mob:FindFirstChild("Humanoid") and mob.Humanoid.Health > 0 then
                     pcall(function()
                         if _G.ques == "Diavolo" then
-                            plr.Character.HumanoidRootPart.CFrame = mob.HumanoidRootPart.CFrame * CFrame.new(plr.Character.HumanoidRootPart.Position, mob.HumanoidRootPart.Position) + Vector3.new(0,_G.dis,0)
+                            plr.Character.HumanoidRootPart.CFrame = mob.HumanoidRootPart.CFrame + Vector3.new(0,_G.dis,0)
+                            plr.Character.HumanoidRootPart.CFrame = CFrame.new(plr.Character.HumanoidRootPart.Position, mob.HumanoidRootPart.Position)
                             game.ReplicatedStorage.Remote.HumonEvent:FireServer("M1", true, plr.Character.HumanoidRootPart.CFrame.LookVector)
                         else
-                            plr.Character.HumanoidRootPart.CFrame = mob.HumanoidRootPart.CFrame * CFrame.new(plr.Character.HumanoidRootPart.Position, mob.HumanoidRootPart.Position) + Vector3.new(0,-_G.dis,0)
+                            plr.Character.HumanoidRootPart.CFrame = mob.HumanoidRootPart.CFrame + Vector3.new(0,-_G.dis,0)
+                            plr.Character.HumanoidRootPart.CFrame = CFrame.new(plr.Character.HumanoidRootPart.Position, mob.HumanoidRootPart.Position)
                             game.ReplicatedStorage.Remote.HumonEvent:FireServer("M1", true, plr.Character.HumanoidRootPart.CFrame.LookVector)
                         end
                     end)
