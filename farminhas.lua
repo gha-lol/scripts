d36d63c72f3356aef572e633617290acb52510e9
gha-lol <85558741+gha-lol@users.noreply.github.com>
2025-02-14 13:59:22 -0300

    Update farminhas.lua

--- farminhas.lua
+++ farminhas.lua
@@ -71,7 +71,7 @@ tab1:CreateToggle("Auto-Farm",false,function(bool)
                 if mob and mob:FindFirstChild("HumanoidRootPart") and mob:FindFirstChild("Humanoid") and mob.Humanoid.Health > 0 then
                     pcall(function()
                         if _G.ques == "Diavolo" then
-                            plr.Character.HumanoidRootPart.CFrame = mob.HumanoidRootPart.CFrame * CFrame.Angles(math.rad(180), 0, 0) + Vector3.new(0,_G.dis,0)
+                            plr.Character.HumanoidRootPart.CFrame = mob.HumanoidRootPart.CFrame * CFrame.Angles(math.rad(280), 0, 0) + Vector3.new(0,_G.dis,0)
                             game.ReplicatedStorage.Remote.HumonEvent:FireServer("M1", true, plr.Character.HumanoidRootPart.CFrame.LookVector)
                         else
                             plr.Character.HumanoidRootPart.CFrame = mob.HumanoidRootPart.CFrame * CFrame.Angles(math.rad(90), 0, 0) + Vector3.new(0,-_G.dis,0)
