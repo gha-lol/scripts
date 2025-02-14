b6a8b799dec71b7c0f67b948cf8b95e34f3c4a90
gha-lol <85558741+gha-lol@users.noreply.github.com>
2025-02-14 01:03:27 -0300

    Update farminhas.lua

--- farminhas.lua
+++ farminhas.lua
@@ -66,13 +66,17 @@ tab1:CreateToggle("Auto-Farm",false,function(bool)
                 game.ReplicatedStorage.Remote.GameEvent:FireServer("Quest", _G.ques)
             else
                 if mob and mob:FindFirstChild("HumanoidRootPart") and mob:FindFirstChild("Humanoid") and mob.Humanoid.Health > 0 then
-                    plr.Character.HumanoidRootPart.CFrame = mob.HumanoidRootPart.CFrame * CFrame.Angles(math.rad(90), 0, 0) + Vector3.new(0,-_G.dis,0)
-                    game.ReplicatedStorage.Remote.HumonEvent:FireServer("M1", true, plr.Character.HumanoidRootPart.CFrame.LookVector)
+                    pcall(function()
+                        plr.Character.HumanoidRootPart.CFrame = mob.HumanoidRootPart.CFrame * CFrame.Angles(math.rad(90), 0, 0) + Vector3.new(0,-_G.dis,0)
+                        game.ReplicatedStorage.Remote.HumonEvent:FireServer("M1", true, plr.Character.HumanoidRootPart.CFrame.LookVector)
+                    end)
                 else
                     if workspace.Enemies:FindFirstChild(plr.Quest.Target.Value) then
                         mob = getMob()
                     else
-                        plr.Character.HumanoidRootPart.CFrame = workspace.Enemies.Pos[plr.Quest.Target.Value].CFrame * CFrame.new(0,-20,0)
+                        pcall(function()
+                            plr.Character.HumanoidRootPart.CFrame = workspace.Enemies.Pos[plr.Quest.Target.Value].CFrame * CFrame.new(0,-20,0)
+                        end)
                     end
                 end
             end
