bcce6b8a5c5aea69890fdf34f4c6c57225a76d97
gha-lol <85558741+gha-lol@users.noreply.github.com>
2025-07-11 19:28:04 -0300

    Update am.lua

--- am.lua
+++ am.lua
@@ -240,7 +240,7 @@ else
 
             if workspace.FX:FindFirstChild("CamGod") then
                 repeat task.wait() until workspace.FX:FindFirstChild("CamGod") == nil
-                task.wait(0.5)
+                task.wait(1)
             end
         end)
         if mob and mob:FindFirstChild("Humanoid") and mob:FindFirstChild("HumanoidRootPart") then
