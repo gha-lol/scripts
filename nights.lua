7b566dd024f02c9e8c45d83071faa38b5a0bfd8d
gha-lol <85558741+gha-lol@users.noreply.github.com>
2025-08-06 02:40:50 -0300

    Update nights.lua

--- nights.lua
+++ nights.lua
@@ -13,6 +13,9 @@ local plr = game.Players.LocalPlayer
 local char = plr.Character
 local inv = plr.Inventory
 
+local selectedWeapon = "Old Axe"
+local selectedEnemy = "Bunny"
+
 function remote(name, args)
     local rem = game.ReplicatedStorage.RemoteEvents:FindFirstChild(name)
     if rem then
@@ -32,12 +35,59 @@ function getSack()
     end
 end
 
+local Weapons
+function updateWeapons()
+    local weapons = {}
+    for i,v in pairs(inv:GetChildren()) do
+        if v:GetAttribute("WeaponDamage") then
+            table.insert(weapons, v.Name)
+        end
+    end
+    Weapons:SetValues(weapons)
+end
+
+local Enemies
+function updateEnemies()
+    local enemies = {}
+    for _,v in pairs(workspace.Characters:GetChildren()) do
+        if not table.find(enemies, v.Name) then
+            table.insert(v.Name)
+        end
+    end
+    Enemies:SetValues(enemies)
+end
+
 --sack:GetAttribute("Capacity")
+
+Weapons = Tabs.Main:CreateDropdown("ItemsList", {Title = "Items List", Values = {}, Multi = false, Default = "Old Axe",})
+Weapons:OnChanged(function(Value)
+    selectedWeapon = Value
+end)
+updateWeapons()
+
+Enemies = Tabs.Main:CreateDropdown("EnemyList", {Title = "Enemy List", Values = {}, Multi = false, Default = "Bunny",})
+Enemies:OnChanged(function(Value)
+    selectedEnemy = Value
+end)
+updateEnemies()
 
 local autoKillToggle = Tabs.Main:CreateToggle("autoKillToggle", {Title = "Auto Kill", Default = false})
 autoKillToggle:OnChanged(function()
     _G.akt = Options.autoKillToggle.Value
     while _G.akt do task.wait()
-        
+        pcall(function()
+            local mob = workspace.Characters:FindFirstChild(selectedEnemy)
+            if mob then
+                char.HumanoidRootPart.CFrame = mob.HumanoidRootPart.CFrame * CFrame.new(0,12,0)
+                char.HumanoidRootPart.Velocity = Vector3.new(0,0,0)
+                remote("EquipItemHandle", {"FireAllClients", inv[selectedWeapon]})
+                remote("ToolDamageObject", {mob, inv[selectedWeapon], "30" .. tostring(plr.PlayerId), char.HumanoidRootPart.CFrame})
+            end
+        end)
     end
 end)
+
+workspace.Characters.ChildAdded:Connect(updateEnemies)
+workspace.Characters.ChildRemoved:Connect(updateEnemies)
+inv.ChildAdded:Connect(updateWeapons)
+inv.ChildRemoved:Connect(updateWeapons)
