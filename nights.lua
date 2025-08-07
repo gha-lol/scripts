7d977e6fc3a3f8e8b2ac4dac2378a0d02d1f5a36
gha-lol <85558741+gha-lol@users.noreply.github.com>
2025-08-07 00:03:45 -0300

    Update nights.lua

--- nights.lua
+++ nights.lua
@@ -169,4 +169,6 @@ end}
 workspace.Characters.ChildAdded:Connect(updateEnemies)
 workspace.Characters.ChildRemoved:Connect(updateEnemies)
 inv.ChildAdded:Connect(updateWeapons)
+
+Window:SelectTab(1)
 inv.ChildRemoved:Connect(updateWeapons)
