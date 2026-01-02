d46330ea4199cf5211157cea23e6fcb5cafe4ada
gha-lol <85558741+gha-lol@users.noreply.github.com>
2026-01-02 00:39:42 -0300

    Create av.lua

---
+++ av.lua
@@ -0,0 +1,23 @@
+local numOrder = {}
+local plr = game.Players.LocalPlayer
+
+plr.PlayerGui.ChildAdded:Connect(function(e)
+    if e.Name == "DialoguePopup" then
+        if e.Holder.Main.DialogueText.ContentText == "I..." then
+            for i,v in pairs(numOrder) do
+                plr.Character.HumanoidRootPart.CFrame = CFrame.new(workspace.Map.Towers["Tower" .. v].Root.Position + Vector3.new(3,0,0))
+                task.wait(.8)
+                game:GetService("VirtualInputManager"):SendKeyEvent(true, "E", false, game)
+                task.wait(.5)
+            end
+            table.clear(numOrder)
+        else
+            local cleaned = e.Holder.Main.DialogueText.ContentText:gsub("%.%.%.$", "")
+            local num = tonumber(cleaned)
+            
+            if num then
+                table.insert(numOrder, tostring(num))
+            end
+        end
+    end
+end)
