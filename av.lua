8ee8e5503a7754ea1026e1aecffa9c413586ba51
gha-lol <85558741+gha-lol@users.noreply.github.com>
2026-01-02 00:51:26 -0300

    Update av.lua

--- av.lua
+++ av.lua
@@ -1,9 +1,10 @@
 local numOrder = {}
 local plr = game.Players.LocalPlayer
 
-plr.PlayerGui.ChildAdded:Connect(function(e)
+plr.PlayerGui.ChildAdded:Connect(function(es)
     if e.Name == "DialoguePopup" then
-        if e.Holder.Main.DialogueText.ContentText == "I..." then
+        if e.Holder.Main.DialogueText.ContentText == "I..." or e.Holder.Main.DialogueText.ContentText == "I" or e.Holder.Main.DialogueText.ContentText == "I.." or e.Holder.Main.DialogueText.ContentText == "I." then
+            warn("No bruh")
             for i,v in pairs(numOrder) do
                 plr.Character.HumanoidRootPart.CFrame = CFrame.new(workspace.Map.Towers["Tower" .. v].Root.Position + Vector3.new(3,0,0))
                 task.wait(.8)
@@ -12,11 +13,14 @@ plr.PlayerGui.ChildAdded:Connect(function(e)
             end
             table.clear(numOrder)
         else
-            local cleaned = e.Holder.Main.DialogueText.ContentText:gsub("%.%.%.$", "")
+            local cleaned = e.Holder.Main.DialogueText.ContentText:gsub("...", "")
+            local cleaned = e.Holder.Main.DialogueText.ContentText:gsub("..", "")
+            local cleaned = e.Holder.Main.DialogueText.ContentText:gsub(".", "")
             local num = tonumber(cleaned)
             
             if num then
                 table.insert(numOrder, tostring(num))
+                warn(tostring(num))
             end
         end
     end
