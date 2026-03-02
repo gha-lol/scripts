e6c044174caec0333fd032a624be3f8522a2251c
gha-lol <85558741+gha-lol@users.noreply.github.com>
2026-03-02 01:45:43 -0300

    Update bl.lua

--- bl.lua
+++ bl.lua
@@ -43,21 +43,15 @@ end
 local plr = game.Players.LocalPlayer
 local char = plr.Character
 
-plr.CharacterAdded:Connect(function(cha)
-    char = cha
-    if t.autofarm or t.autoraid then
-        align.Attachment0 = char.HumanoidRootPart.RootAttachment
-        orient.Attachment0 = char.HumanoidRootPart.RootAttachment
-    end
-end)
-
 if char == nil and plr.PlayerGui:FindFirstChild("Main Menu") then
     game:GetService("GuiService").SelectedObject = plr.PlayerGui["Main Menu"].Buttons["Quick Play"]
     game:GetService("VirtualInputManager"):SendKeyEvent(true, "Return", false, game)
     task.wait()
     game:GetService("VirtualInputManager"):SendKeyEvent(false, "Return", false, game)
     
-    repeat task.wait(.1) until char
+    repeat task.wait(.1) until plr.Character
+
+    char = plr.Character
 end
 
 
@@ -83,6 +77,14 @@ orient.MaxTorque = math.huge
 orient.Responsiveness = 200
 orient.Attachment0 = char.HumanoidRootPart.RootAttachment
 orient.Attachment1 = part.Attachment
+
+plr.CharacterAdded:Connect(function(cha)
+    char = cha
+    if t.autofarm or t.autoraid then
+        align.Attachment0 = char.HumanoidRootPart.RootAttachment
+        orient.Attachment0 = char.HumanoidRootPart.RootAttachment
+    end
+end)
 
 
 -- Functions
