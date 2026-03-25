4b1aad45e3c949b1a5b3a2d23409f2f84c2f60d9
gha-lol <85558741+gha-lol@users.noreply.github.com>
2026-03-25 15:27:06 -0300

    Create sp.lua

---
+++ sp.lua
@@ -0,0 +1,267 @@
+local Library = loadstring(game:HttpGetAsync("https://github.com/ActualMasterOogway/Fluent-Renewed/releases/latest/download/Fluent.luau"))()
+local Window = Library:CreateWindow{Title = "sp", SubTitle = "by gha", TabWidth = 160, Size = UDim2.fromOffset(1500, 900), Resize = true,MinSize = Vector2.new(470, 380),Acrylic = true,Theme = "Dark",MinimizeKey = Enum.KeyCode.Q}
+
+local Tabs = {
+    AutoFarm = Window:CreateTab{
+        Title = "AutoFarm",
+        Icon = "phosphor-users-bold"
+    }
+}
+
+local noSave = {}
+local t = {
+    autofarm = false,
+    autoboss = false,
+    autoloopswords = false,
+    autoloopmelees = false,
+    autolooppowers = false,
+    distance = 8,
+    selectedEnemy = "asdasdas",
+    selectedSwords = {},
+    selectedMelees = {},
+    selectedPowers = {},
+    selectedBosses = {}
+}
+
+
+-- Variables
+
+local plr = game.Players.LocalPlayer
+local char = plr.Character or plr.CharacterAdded:Wait()
+
+local allBosses = {}
+local Options = Library.Options
+local UIElements = {}
+
+
+-- Important
+
+local part = Instance.new("Part", workspace)
+part.Transparency = 1
+part.Anchored = true
+part.Massless = true
+part.CanCollide = false
+part.CFrame = char.HumanoidRootPart.CFrame
+Instance.new("Attachment", part)
+
+local align = Instance.new("AlignPosition", part)
+align.MaxForce = 99e99
+align.MaxVelocity = 500
+align.Responsiveness = 200
+align.Attachment0 = char.HumanoidRootPart.RootAttachment
+align.Attachment1 = part.Attachment
+
+local orient = Instance.new("AlignOrientation", part)
+orient.MaxTorque = math.huge
+orient.Responsiveness = 200
+orient.Attachment0 = char.HumanoidRootPart.RootAttachment
+orient.Attachment1 = part.Attachment
+
+plr.CharacterAdded:Connect(function(cha)
+    char = cha
+    align.Attachment0 = nil
+    orient.Attachment0 = nil
+end)
+
+
+-- Functions
+
+function createElement(tab, elementType, id, data, callback)
+    local element
+
+    if elementType == "Toggle" then
+		element = tab:CreateToggle(id, data)
+	elseif elementType == "Paragraph" then
+		element = tab:CreateParagraph(id, data)
+	elseif elementType == "Button" then
+		element = tab:CreateButton(data)
+	elseif elementType == "Dropdown" then
+		element = tab:CreateDropdown(id, data)
+    elseif elementType == "Input" then
+        element = tab:CreateInput(id, data)
+	end
+
+    if not element then
+        warn("Failed to create element:", elementType, id)
+        return nil
+    end
+
+    if id and elementType ~= "Paragraph" then
+        UIElements[id] = element
+    end
+
+    if callback and element.OnChanged then
+        element:OnChanged(function(...)
+            callback(Options[id], ...)
+        end)
+    end
+
+    return element
+end
+
+function setAligns(a)
+    pcall(function()
+        if a then
+            align.Attachment0 = char.HumanoidRootPart.RootAttachment
+            orient.Attachment0 = char.HumanoidRootPart.RootAttachment
+        else
+            align.Attachment0 = nil
+            orient.Attachment0 = nil
+        end
+    end)
+end
+
+function getClosestIsland(enemy)
+    local distance = math.huge
+    local toTp
+    local spawnCrystal
+
+    for i,v in pairs(workspace:GetChildren()) do
+        if v.Name:match("Island") and v:IsA("Folder") then
+            local dis
+            for _,spawnp in pairs(v:GetChildren()) do if spawnp.Name:match("SpawnPointCrystal") then spawnCrystal = spawnp dis = (spawnp:GetPivot().Position - enemy:GetPivot().Position).Magnitude end end
+
+            if dis < distance then
+                toTp = v
+                distance = dis
+            end
+        end
+    end
+
+    local e,_ = string.gsub(toTp.Name, "Island", "")
+    for i,v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.TeleportUI.MainFrame.Frame.Content.Holder:GetChildren()) do
+        if v.Name:match(e) then
+            e,_ = v.Name:gsub("Teleport_","")
+            break
+        end
+    end
+
+    return toTp, distance, spawnCrystal
+end
+
+function checkAlive(enemy)
+    if enemy:FindFirstChild("Humanoid") and enemy.Humanoid.Health > 0 then
+        return true
+    end
+    return false
+end
+
+function getEnemy(bool, getAll)
+    local returner, a, b, c = nil, nil, nil, nil
+
+    if bool == "autoboss" then
+        returner = allBosses
+
+        for i,v in pairs(workspace.NPCs:GetChildren()) do
+            if checkAlive(v) and v:FindFirstChild("Boss") and table.find(t.selectedBosses, v.Name) and not getAll then
+                returner = v
+                a, b, c = getClosestIsland(v)
+                break
+            elseif v:FindFirstChild("Boss") and getAll then
+                if not table.find(returner, v.Name) then
+                    table.insert(returner, v.Name)
+                end
+            end
+        end
+
+    elseif bool = "autofarm" then
+        returner = {}
+
+        for i,v in pairs(workspace.NPCs:GetChildren()) do
+            if checkAlive(v) and v.Name:match(t.selectedEnemy) and not getAll then
+                returner = v
+                a, b, c = getClosestIsland(v)
+                break
+            elseif getAll then
+                if not table.find(returner, v.Name) then
+                    table.insert(returner, v.Name)
+                end
+            end
+        end
+
+    end
+
+    allBosses = returner
+    return returner, a, b, c
+end
+
+local isAutoFarming = false
+function autofarm(bool)
+    if isAutoFarming then return end
+    isAutoFarming = true
+
+    local enemy, islandName, distance, spawnCrystal = nil, nil, nil, nil
+    local posY, cfAng = t.distance, -90
+
+    while t[bool] do task.wait()
+        if enemy and checkAlive(enemy) and plr:DistanceFromCharacter(enemy:GetPivot().Position) <= distance + 20 then
+            setAligns(true)
+
+            part.CFrame = CFrame.new(enemy:GetPivot().Position + Vector3.new(0,posY,0)) * CFrame.Angles(math.rad(cfAng),0,0)
+        elseif enemy and checkAlive(enemy) then
+            setAligns(false)
+
+            repeat task.wait(.1)
+                game:GetService("ReplicatedStorage").Remotes.TeleportToPortal:FireServer(islandName)
+            until plr:DistanceFromCharacter(enemy:GetPivot().Position) <= distance + 20
+
+            if spawnCrystal then
+                local prox = spawnCrystal:FindFirstChild("CheckpointPrompt", true)
+                
+                for i=1,4 do
+                    char:PivotTo(spawnCrystal:GetPivot())
+                    fireproximityprompt(prox)
+                    task.wait(.1)
+                end
+            end
+        else
+            enemy, islandName, distance, spawnCrystal = getEnemy(bool)
+        end
+    end
+
+    isAutoFarming = false
+end
+
+
+-- Ui
+
+-- AUTO BOSSES SECTION
+
+createElement(Tab.AutoFarm, "Paragraph", "Alignss", {Title = "Auto Bosses Section", Content = "", TitleAlignment = "Middle", ContentAlignment = Enum.TextXAlignment.Center})
+
+createElement(Tab.AutoFarm, "Toggle", "autobossesToggle", {Title = "Auto Bosses", Default = t.autoboss}, function(self)
+    t.autoboss = self.Value
+    autofarm("autoboss")
+end)
+
+createElement(Tabs.AutoFarm, "Dropdown", "selectBossesDropdown", {Title = "Bosses To Kill", Multi = true, Searchable = true, FocusSearch = false, Values = getEnemy("autoboss", true), Default = t.selectedBosses}, function(self, val)
+    local tab = {}
+
+    for i,v in pairs(val) do
+        table.insert(tab, i)
+    end
+
+    t.selectedBosses = tab
+end)
+
+createElement(Tabs.AutoFarm, "Button", nil, {Title = "Update Bosses List", Callback = function()
+    UIElements.selectBossesDropdown:SetValues(getEnemy("autoboss", true))
+end})
+
+
+-- AUTOFARM SECTION
+
+createElement(Tab.AutoFarm, "Paragraph", "Alignss", {Title = "Auto Farm Section", Content = "", TitleAlignment = "Middle", ContentAlignment = Enum.TextXAlignment.Center})
+
+createElement(Tab.AutoFarm, "Toggle", "autofarmToggle", {Title = "Auto Farm", Default = t.autofarm}, function(self)
+    t.autofarm = self.Value
+    autofarm("autofarm")
+end)
+
+createElement(Tabs.AutoFarm, "Dropdown", "selectEnemyDropdown", {Title = "Enemy To Hold", Searchable = true, FocusSearch = false, Values = getEnemy("autofarm", true), Default = t.selectedEnemy}, function(self, val)
+    t.selectedEnemy = val
+end)
+
+createElement(Tabs.AutoFarm, "Button", nil, {Title = "Update Enemy List", Callback = function()
+    UIElements.selectEnemyDropdown:SetValues(getEnemy("autofarm", true))
+end})
