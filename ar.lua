064fd9087b991662e47502ade9d126d24a922717
gha-lol <85558741+gha-lol@users.noreply.github.com>
2026-03-28 02:28:03 -0300

    Update ar.lua

--- ar.lua
+++ ar.lua
@@ -3,26 +3,39 @@ local Window = Library:CreateWindow{Title = "ar", SubTitle = "by gha", TabWidth 
 
 local Tabs = {
     Main = Window:CreateTab{
-        Title = "Main",
+        Title = "Lobby",
+        Icon = "phosphor-users-bold"
+    },
+    Auto = Window:CreateTab{
+        Title = "Auto",
         Icon = "phosphor-users-bold"
     }
 }
+
+local t = {
+    fuseDropdown1,
+    fuseDropdown2,
+    selectedMainFuse,
+    selectedFodderFuse,
+    selectedStat = "Damage"
+}
+
+
+-- Variables
+
 local Options = Library.Options
-local Remotes = game.ReplicatedStorage.Remotes
+local UIElements = {}
+local Remotes = game.ReplicatedStorage:FindFirstChild("Remotes")
+local gameStatus = workspace:FindFirstChild("GameStatus")
 
 local lastUnits = {}
 
-local fuseDropdown1
-local fuseDropdown2
-local selectedMainFuse
-local selectedFodderFuse
-local selectedStat = "Damage"
 
 -- Functions
 
 function updateAllDropdowns()
-    fuseDropdown1:SetValues(lastUnits)
-    fuseDropdown2:SetValues(lastUnits)
+    UIElements.fuseDropdown1:SetValues(lastUnits)
+    UIElements.fuseDropdown2:SetValues(lastUnits)
 end
 
 function addList(unit)
@@ -40,69 +53,157 @@ function addList(unit)
     end
 end
 
+function createElement(tab, elementType, id, data, callback)
+    local element
+
+    -- Criador dinâmico
+    if elementType == "Toggle" then
+		element = tab:CreateToggle(id, data)
+	elseif elementType == "Paragraph" then
+		element = tab:CreateParagraph(id, data)
+	elseif elementType == "Button" then
+		element = tab:CreateButton(data)
+	elseif elementType == "Dropdown" then
+		element = tab:CreateDropdown(id, data)
+    elseif elementType == "Input" then
+        element = tab:CreateInput(id, data)
+	end
+
+    if not element then
+        warn("Failed to create element:", elementType, id)
+        return nil
+    end
+
+    -- Salva global
+    if id and elementType ~= "Paragraph" then
+        UIElements[id] = element
+    end
+
+    -- Auto OnChanged
+    if callback and element.OnChanged then
+        element:OnChanged(function(...)
+            callback(Options[id], ...)
+        end)
+    end
+
+    return element
+end
+
+function card(vezes, a)
+    local con = getconnections(game:GetService("ReplicatedStorage").Remotes.Card.Main.OnClientEvent)
+    for i,v in pairs(con) do v:Disable() end
+
+    for i=1,vezes do
+        game:GetService("ReplicatedStorage").Remotes.Card.Main:FireServer("Vote",a, 1)
+    end
+
+    delay(5,function()
+        for i,v in pairs(con) do v:Enable() end 
+    end)
+end
+
 
 -- Hook
 
-local namecall
-namecall = hookmetamethod(game,"__namecall",function(self,...)
-    local args = {...}
-    local method = getnamecallmethod():lower()
-    pcall(function()
-        if not checkcaller() and self == Remotes.Unit.Lock and method == "fireserver" then
-            if not table.find(lastUnits, args[1]) then
-                table.insert(lastUnits, args[1])
+if game.PlaceId == 85535589075948 then
+    local namecall
+    namecall = hookmetamethod(game,"__namecall",function(self,...)
+        local args = {...}
+        local method = getnamecallmethod():lower()
+        pcall(function()
+            if not checkcaller() and self == Remotes.Unit.Lock and method == "fireserver" then
+                if not table.find(lastUnits, args[1]) then
+                    table.insert(lastUnits, args[1])
+                end
             end
-        end
+        end)
+        return namecall(self,...)
     end)
-    return namecall(self,...)
+end
+
+
+
+-- Main / Lobby
+
+createElement(Tabs.Main, "Paragraph", "AlignedParagraph", {Title = "Fuse Unit", Content = "", TitleAlignment = "Middle", ContentAlignment = Enum.TextXAlignment.Center})
+
+createElement(Tabs.Main, "Dropdown", "fuseDropdown1", {Title = "Main Unit", Values = {}, Multi = false, Default = ""}, function(self, Value)
+    t.selectedMainFuse = Value
 end)
 
-
--- Main
-
-local parag
-parag = Tabs.Main:CreateParagraph("Aligned Paragraph", {Title = "Fuse Unit", Content = "", TitleAlignment = "Middle", ContentAlignment = Enum.TextXAlignment.Center})
-
-fuseDropdown1 = Tabs.Main:CreateDropdown("fuseDropdown1", {Title = "Main Unit", Values = {}, Multi = false, Default = ""})
-fuseDropdown1:OnChanged(function(Value)
-    selectedMainFuse = Value
+createElement(Tabs.Main, "Dropdown", "fuseDropdown2", {Title = "Fodder Unit", Values = {}, Multi = false, Default = ""}, function(self, Value)
+    t.selectedFodderFuse = Value
 end)
 
-fuseDropdown2 = Tabs.Main:CreateDropdown("fuseDropdown2", {Title = "Fodder Unit", Values = {}, Multi = false, Default = ""})
-fuseDropdown2:OnChanged(function(Value)
-    selectedFodderFuse = Value
+createElement(Tabs.Main, "Dropdown", "statDropdown", {Title = "Stat", Values = {"Damage", "Cooldown", "Range"}, Multi = false, Default = "Damage"}, function(self, Value)
+    t.selectedStat = Value
 end)
 
-statDropdown = Tabs.Main:CreateDropdown("statDropdown", {Title = "Stat", Values = {"Damage", "Cooldown", "Range"}, Multi = false, Default = "Damage"})
-statDropdown:OnChanged(function(Value)
-    selectedStat = Value
-end)
-
-Tabs.Main:CreateButton{Title = "Pass Stat", Description = "", Callback = function()
+createElement(Tabs.Main, "Button", nil, {Title = "Pass Stat", Description = "", Callback = function()
     spawn(function()
         for i=1,1000 do
-            Remotes.StatTransfer.Transfer:FireServer(selectedFodderFuse, selectedMainFuse, {selectedStat})
+            Remotes.StatTransfer.Transfer:FireServer(t.selectedFodderFuse, t.selectedMainFuse, {t.selectedStat})
         end
     end)
-end}
+end})
 
-Tabs.Main:CreateButton{Title = "Fuse Unit", Description = "", Callback = function()
+createElement(Tabs.Main, "Button", nil, {Title = "Fuse Unit", Description = "", Callback = function()
     local tab = {}
     for i=1,999 do
-        table.insert(tab, selectedFodderFuse)
+        table.insert(tab, t.selectedFodderFuse)
     end
 
     spawn(function()
         for i=1,9999 do
-            Remotes.Unit.Fuse:FireServer(selectedMainFuse, tab)
+            Remotes.Unit.Fuse:FireServer(t.selectedMainFuse, tab)
         end
     end)
-end}
+end})
 
-Tabs.Main:CreateButton{Title = "Update Recent Units Table", Description = "", Callback = function()
+createElement(Tabs.Main, "Button", nil, {Title = "Update Recent Units Table", Description = "", Callback = function()
     updateAllDropdowns()
-end}
-Tabs.Main:CreateButton{Title = "Clear Recent Units Table", Description = "", Callback = function()
+end})
+
+createElement(Tabs.Main, "Button", nil, {Title = "Clear Recent Units Table", Description = "", Callback = function()
     lastUnits = {}
     updateAllDropdowns()
-end}
+end})
+
+
+
+-- Game / Playing
+
+local consAutoWitch = {}
+createElement(Tabs.Auto, "Toggle", "AutoWitchToggle", {Title = "Auto Witch Inf", Default = false}, function(self)
+    for _,v in pairs(consAutoWitch) do v:Disconnect() end
+
+    if self.Value then
+        local alreadyGotCards = false
+
+        local con1 = gameStatus.GospelPageEarn.Changed:Connect(function()
+            if gameStatus.GospelPageEarn.Value ~= 0 then
+                Remotes.Replay:FireServer("Setting")
+            end
+        end)
+
+        local con2 = Remotes.ChangeRewardFrame.OnClientEvent:Connect(function()
+            task.wait(.5)
+            alreadyGotCards = false
+            Remotes.Replay:FireServer()
+        end)
+
+        local con3 = gameStatus.Wave:Connect(function()
+            if gameStatus.Wave.Value >= 3 and not alreadyGotCards then
+                alreadyGotCards = true
+
+                card(500, "Distorted Timing III")
+                task.wait(6)
+                card(1000, "Swift Notes III")
+            end
+        end)
+
+        table.insert(consAutoWitch, con1)
+        table.insert(consAutoWitch, con2)
+        table.insert(consAutoWitch, con3)
+    end
+end)
