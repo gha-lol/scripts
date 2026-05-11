ed7471234e2ed7803ac85bbf79a95094434ccc60
gha-lol <85558741+gha-lol@users.noreply.github.com>
2026-05-11 01:14:48 -0300

    Update tdx.lua

--- tdx.lua
+++ tdx.lua
@@ -155,6 +155,42 @@ function antiAfk()
     end
 end
 
+function fpsBoost()
+    local Lighting = game:GetService("Lighting")
+    local Terrain = workspace:FindFirstChildWhichIsA("Terrain")
+	Terrain.WaterWaveSize = 0
+	Terrain.WaterWaveSpeed = 0
+	Terrain.WaterReflectance = 0
+	Terrain.WaterTransparency = 1
+	Lighting.GlobalShadows = false
+	Lighting.FogEnd = 9e9
+	Lighting.FogStart = 9e9
+	settings().Rendering.QualityLevel = 1
+	for _, v in pairs(game:GetDescendants()) do
+		if v:IsA("BasePart") then
+			v.CastShadow = false
+			v.Material = "Plastic"
+			v.Reflectance = 0
+			v.BackSurface = "SmoothNoOutlines"
+			v.BottomSurface = "SmoothNoOutlines"
+			v.FrontSurface = "SmoothNoOutlines"
+			v.LeftSurface = "SmoothNoOutlines"
+			v.RightSurface = "SmoothNoOutlines"
+			v.TopSurface = "SmoothNoOutlines"
+		elseif v:IsA("Decal") then
+			v.Transparency = 1
+			v.Texture = ""
+		elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
+			v.Lifetime = NumberRange.new(0)
+		end
+	end
+	for _, v in pairs(Lighting:GetDescendants()) do
+		if v:IsA("PostEffect") then
+			v.Enabled = false
+		end
+	end
+end
+
 
 -- Code
 
@@ -184,6 +220,8 @@ createElement(Tabs.Misc, "Toggle", "toggleAntiAfk", {Title = "Anti Afk", Default
     t.antiAfk = self.Value
     antiAfk()
 end)
+
+createElement(Tabs.Misc, "Button", nil, {Title = "Boost Fps", Description = "", Callback = fpsBoost})
 
 
 --     File Tab
